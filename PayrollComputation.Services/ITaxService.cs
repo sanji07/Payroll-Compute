cbcb6d09615d374c43005a01d9b72875f5a68417
sanjithAnbazhagan <sanzk94@gmail.com>
2020-05-25 23:03:36 +1000

    adding Tax functions

---
+++ PayrollComputation.Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayrollComputation.Services
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
